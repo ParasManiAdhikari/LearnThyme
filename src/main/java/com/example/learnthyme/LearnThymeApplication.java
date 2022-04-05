c3cc728987e3356392e6c7d84a32e0ca1cc9668e
Paras Adhikari <paras.adhikari@stud.th-luebeck.de>
2022-04-05 15:44:43 +0200

    Initial commit

---
+++ src/main/java/com/example/learnthyme/LearnThymeApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnthyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnThymeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnThymeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnthyme/LearnThymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnthyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnThymeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
